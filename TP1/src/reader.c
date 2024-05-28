c1f50d7df68d4d0d67cbe3a9234f37a4675bbe58
martin_paura_bersan <mpaurabe@itba.edu.ar>
2024-05-28 15:42:03 -0300

    Funcionando el envio, falta las signals

--- TP1/src/reader.c
+++ TP1/src/reader.c
@@ -1,3 +1,18 @@
+//-----------------------------------------------------------------------------------------------------------
+//cd C:\Users\mpaur\OneDrive\Documentos\ITBA\MESE\SOPG\Practica\SOPG_Practica\app\src>
+//C:\Users\mpaur\OneDrive\Documentos\ITBA\MESE\SOPG\Practica\SOPG_Practica\app\src>start prueba.exe
+//-----------------------------------------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------------------------------------
+//En Linux
+//se compila por linea de comando con $ gcc -Wall -pedantic ./src/reader.c -o ./build/reader 
+//se ejecuta con $ .\a.out
+//para ver los procesos en otro terminal correr 
+//& watch -n 1 "ps -elf | grep a.out"
+//la "S" se ve en general porque gran parte del tiempo esta en sleep()
+//cuando lo paramos se pone con una "T"
+//-----------------------------------------------------------------------------------------------------------
 #include <stdio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -21,9 +36,32 @@ int main(void)
     do {
         if ((num = read(fd, s, 300)) == -1)
             perror("read");
-        else {
+        else 
+        {
             s[num] = '\0';
             printf("reader: read %d bytes: \"%s\"\n", num, s);
+            s[num+1] = '\n';
+            
+            if(0==strncmp(s,"DATA:",5))
+            {
+                int fddata = open("Log.txt",O_CREAT|O_RDWR|O_APPEND);
+                write(fddata,s,num+2);
+                close(fddata);
+            }
+            else if(0==strncmp(s,"SIGN:1",6))
+            {
+                int fdsign = open("Sign.txt",O_CREAT|O_RDWR|O_APPEND);
+                write(fdsign,s,num+2);
+                close(fdsign);
+            }
+            else if(0==strncmp(s,"SIGN:2",6))
+            {
+                int fdsign = open("Sign.txt",O_CREAT|O_RDWR|O_APPEND);
+                write(fdsign,s,num+2);
+                close(fdsign);
+            }   
+            else
+                perror("Error de Datos");
         }
     }
     while (num > 0);
--- TP1/src/writer.c
+++ TP1/src/writer.c
@@ -6,33 +6,50 @@
 
 //-----------------------------------------------------------------------------------------------------------
 //En Linux
-//se compila por linea de comando con $ gcc -Wall -pedantic ./src/clase3_eje2.c -o ./build/clase3_eje2 
+//se compila por linea de comando con $ gcc -Wall -pedantic ./src/writer.c -o ./build/writer
 //se ejecuta con $ .\a.out
 //para ver los procesos en otro terminal correr 
 //& watch -n 1 "ps -elf | grep a.out"
 //la "S" se ve en general porque gran parte del tiempo esta en sleep()
 //cuando lo paramos se pone con una "T"
 //-----------------------------------------------------------------------------------------------------------
-
-
 #include <stdio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void sigcuser_handler(int sig) {
+    pid_t pidchild;
+    int wstatus;
+ 	pidchild = wait(&wstatus);
+    printf(" PARENT: I'm on handler %d\n", wstatus);
+    return;
+}
 
 
 //Execute on Terminal ./writer
 
 #define FIFO_NAME "myfifo"
+#define STDIN_FD            0
+#define STDOUT_FD           1
 
 int main(void)
 {
+    printf("PROCESO WRITER PID = %d\n", getpid());
     mkfifo(FIFO_NAME, 0666);
-
-    printf("waiting for readers...\n");
     int fd = open(FIFO_NAME, O_WRONLY);
     printf("got a reader--type some stuff\n");
+
+    pid_t pid;
+	struct sigaction sa;
+	sa.sa_handler = sigcuser_handler;
+	sa.sa_flags = 0; //SA_RESTART;
+	sigemptyset(&sa.sa_mask);
 
     char s[300];
     int num;
